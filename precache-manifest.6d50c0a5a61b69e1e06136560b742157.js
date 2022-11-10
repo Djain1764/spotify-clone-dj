33fd357001d949f7a35ec113d3c7480aa98c42fb
Divyansh Jain <djain1764@gmail.com>
2022-11-10 12:14:27 +0530

    Updates

---
+++ precache-manifest.6d50c0a5a61b69e1e06136560b742157.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f33bef84359d666e04cb7eec6b2fb237",
+    "url": "/spotify-clone-dj/index.html"
+  },
+  {
+    "revision": "11623b2d4b69916397b8",
+    "url": "/spotify-clone-dj/static/css/main.c8e2e44e.chunk.css"
+  },
+  {
+    "revision": "096e207adc2b9230dd55",
+    "url": "/spotify-clone-dj/static/js/2.7dbb9900.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/spotify-clone-dj/static/js/2.7dbb9900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11623b2d4b69916397b8",
+    "url": "/spotify-clone-dj/static/js/main.3847d157.chunk.js"
+  },
+  {
+    "revision": "96d854c3ec993e09c9ac",
+    "url": "/spotify-clone-dj/static/js/runtime-main.5bada56e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/spotify-clone-dj/precache-manifest.6d50c0a5a61b69e1e06136560b742157.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spotify-clone-dj/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
